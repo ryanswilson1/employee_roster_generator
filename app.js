934a18d44ffafbffd19559c095d5a620d24b5409
Ryan Wilson <ryanswilson1@gmail.com>
2020-10-21 17:46:27 -0400

    created team generator func

--- app.js
+++ app.js
@@ -10,6 +10,8 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const Team = [];
+
 function managerResponse() {
     inquirer.prompt([
         {
@@ -30,14 +32,41 @@ function managerResponse() {
         {
             type: "input",
             name: "officeNumber",
-            message: "Enter manager office number."
+            message: "Enter manager telephone number."
         }
     ]).then(answers => {
-        let { name, id, email, officeNumber } = responses;
-        let manager = new Manager(name, id, email, officeNumber);
+        let { name, id, email, phoneNumber } = responses;
+        let manager = new Manager(name, id, email, phoneNumber);
 
         teamMembers.push(manager);
 
         generateTeam();
+    })
+}
+
+function generateTeam() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "command",
+            message: "Add new team members?",
+            choices: ["Add Engineer", "Add Intern", "Team Complete"]
+        }
+    ]).then(answers => {
+        memberType = answers.command;
+
+        switch (memberType) {
+            case "Add Engineer":
+                getEngineer();
+                break;
+
+            case "Add Intern":
+                getIntern();
+                break;
+
+            case "Team Complete":
+                createTeam();
+                break;
+        }
     })
 }
